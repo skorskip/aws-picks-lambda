648c140957b53c991df244a1c823472e79a7728a
skorskip <skorskip@umich.edu>
2020-09-11 20:38:16 -0400

    Remove using env variables, npm didnt like it

--- league/app/model/db.js
+++ league/app/model/db.js
@@ -1,15 +1,10 @@
 'user strict';
 
 var mysql = require('mysql');
+var config = require('../../config.json');
 
 //local mysql db connection
-var connection = mysql.createConnection({
-    host : process.env.DB_HOST,
-    user : process.env.DB_USER,
-    password : process.env.DB_PASSWORD,
-    database : process.env.DB_DATABASE,
-    port : process.env.DB_PORT
-});
+var connection = mysql.createConnection(config.database);
 
 connection.connect(function(err){
     if (err) throw err;
