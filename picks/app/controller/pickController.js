396d95c4274edbfca66120c0b640d9c8d744050b
skorskip <skorskip@umich.edu>
2020-10-13 20:19:02 -0400

    Fix user param

--- picks/app/controller/pickController.js
+++ picks/app/controller/pickController.js
@@ -3,7 +3,7 @@
 var Pick = require('../model/pickModel.js');
 
 exports.getUsersPicksByWeek = function(req, res) {
-    Pick.getUsersPicksByWeek(req.query.userId, req.query.season, req.query.week, req.query.seasonType, function(err, picks) {
+    Pick.getUsersPicksByWeek(req.query.user, req.query.season, req.query.week, req.query.seasonType, function(err, picks) {
         if(err) return res.status(500).send({error: true, message: "Error retrieving users picks", content: err});
         res.json(picks);
     });
