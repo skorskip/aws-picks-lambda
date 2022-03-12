8df857bc151c6fd93f7a74839b6e0debc7d953bf
skorskip <skorskip@umich.edu>
2022-03-12 13:28:32 -0500

    Fix get week api

--- week/app/controller/weekController.js
+++ week/app/controller/weekController.js
@@ -1,8 +1,8 @@
 'use strict';
 var Week = require('../model/weekModel.js');
 
-exports.getWeek = function(req, res) {
-    var week = Week.getWeek(req.query.season, 
+exports.getWeek = async function(req, res) {
+    var week = await Week.getWeek(req.query.season, 
         req.query.week, 
         req.query.seasonType,
         req.headers.authorization);
